97507aae10c6b791f7b91236dae9be10e1df58f0
Jitesh Kumar <jitesbharti@gmail.com>
2022-01-03 16:34:28 +0000

    Sync LeetCode submission - Power of Two (cpp)

---
+++ problems/power_of_two/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        
+        if(n<=0) return false;
+        
+        if(n==1 ) return true;
+        
+        if(n%2==0)
+            
+          return   isPowerOfTwo( n/2);
+        
+        
+         else return false;
+        
+    }
+};
