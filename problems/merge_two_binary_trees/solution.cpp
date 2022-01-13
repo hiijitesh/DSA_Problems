0145c209eb8d74c573fda90e90ad9caf35720e48
Jitesh Kumar <jitesbharti@gmail.com>
2022-01-13 16:08:34 +0000

    Sync LeetCode submission - Merge Two Binary Trees (cpp)

---
+++ problems/merge_two_binary_trees/solution.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* merge( TreeNode* root1, TreeNode* root2){
+        
+        if( root1==NULL && root2==NULL ) return NULL;
+        
+        if( root1 == NULL ) return root2;
+        
+        if( root2 == NULL ) return root1;
+        
+        TreeNode* newNode = new TreeNode(root1->val + root2->val);
+            
+        //      int newval = root1->val + root2->val;
+        // TreeNode* newnode = new TreeNode(newval);
+        
+        newNode->left= merge( root1->left, root2->left);
+        
+        newNode->right= merge(root1->right, root2->right);
+        
+        return newNode;
+
+    }
+    
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        
+        return merge ( root1, root2);
+        
+    }
+};
