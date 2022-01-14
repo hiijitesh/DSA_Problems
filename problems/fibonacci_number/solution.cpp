e722c12843d6bc3f520f4495fc484ddb707f4563
Jitesh Kumar <jitesbharti@gmail.com>
2022-01-14 17:01:55 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
-    int fib(int n) {
+    int myfib( int n,vector<int> &dp){
         
-        if ( n==0 ) return 0;
+        if( n==0 ) return 0;
         
         if( n==1 ) return 1;
         
-        return fib(n-1)+ fib(n-2);
+        if ( dp[n] != -1) return dp[n];
+        
+        return dp[n]= myfib(n-1,dp)+ myfib(n-2,dp);
+    }
+    
+    int fib(int n ) {
+        
+        vector<int> dp(n+1, -1);
+        
+        return myfib( n,dp);
+        
+        // recursive solution
+//         if ( n==0 ) return 0;
+        
+//         if( n==1 ) return 1;
+        
+//         return fib(n-1)+ fib(n-2);
         
     }
 };
