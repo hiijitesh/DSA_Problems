1f418cde046a2cfb47e4d0f85891ee8587f0df93
Jitesh Kumar <jitesbharti@gmail.com>
2022-01-20 12:38:15 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (cpp)

--- problems/best_time_to_buy_and_sell_stock/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock/solution.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int minPrice= INT_MAX;
-        int profitMax= 0;
-        int Difference;
-        for(int i=0; i<n;i++){
-            if(prices[i]<minPrice)
-                minPrice=prices[i];
-            // else
-            //     continue;
-            Difference=prices[i]-minPrice;
-            if(profitMax<Difference)
-                profitMax=Difference;
-            else
-                continue;
+    
+    int bestTime ( vector<int> & prices, int currDay, bool canBuy,int trans, vector<vector<vector<int>>> &dp){
+        
+        if (currDay >= prices.size() ) return 0; 
+        if ( trans <=0 ) return 0;
+        
+        if(dp[currDay][canBuy][trans] != -1) return dp[currDay][canBuy][trans];
+        
+        if(canBuy) {
+              int  idle = bestTime(prices,currDay+1,canBuy,trans,dp);
+              int  buy = -prices[currDay] + bestTime(prices, currDay+1,false,trans,dp);
+              return dp[currDay][canBuy][trans] = max(idle,buy);          
         }
-        return profitMax;
+        else{
+             int idle = bestTime(prices,currDay+1,canBuy,trans,dp);
+             int sell = prices[currDay] + bestTime( prices,currDay+1,true,trans-1,dp);
+             return dp[currDay][canBuy][trans] = max(idle,sell);
+        }    
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(2,-1)));
+        return bestTime( prices,0,true, 1,dp);
         
     }
 };
