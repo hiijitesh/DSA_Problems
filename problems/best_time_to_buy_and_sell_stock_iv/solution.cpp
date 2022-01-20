14ecc3fba291f3350d0207977d2c373439fb5d12
Jitesh Kumar <jitesbharti@gmail.com>
2022-01-20 13:07:30 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock IV (cpp)

---
+++ problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    int bestTime ( vector<int> & prices, int currDay, bool canBuy,int trans, vector<vector<vector<int>>> &dp){
+        
+        if (currDay >= prices.size() ) return 0; 
+        if ( trans <=0 ) return 0;
+        
+        if(dp[currDay][canBuy][trans] != -1) return dp[currDay][canBuy][trans];
+        
+        if(canBuy) {
+              int  idle = bestTime(prices,currDay+1,canBuy,trans,dp);
+              int  buy = -prices[currDay] + bestTime(prices, currDay+1,false,trans,dp);
+              return dp[currDay][canBuy][trans] = max(idle,buy);          
+        }
+        else{
+             int idle = bestTime(prices,currDay+1,canBuy,trans,dp);
+             int sell = prices[currDay] + bestTime( prices,currDay+1,true,trans-1,dp);
+             return dp[currDay][canBuy][trans] = max(idle,sell);
+        }    
+    }
+    
+    int maxProfit(int k,vector<int>& prices) {
+        
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return bestTime( prices,0,true, k,dp);
+    }       
+};
