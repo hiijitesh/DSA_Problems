ece3eea6e9b50de72313304166e9c354c5a8e24c
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-03-01 20:57:54 +0530

    Time: 32 ms (38.83%), Space: 15.3 MB (33.96%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,41 +1,40 @@
 class Solution {
-    private:
+public:
     
-    void sourceToPath (vector<vector<int>>& graph, int currNode,vector<bool> & visited,int n, vector<int> &currPath,vector<vector<int>> &ans){
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         
-        if( currNode == n-1){
-            
+        vector<vector<int> > ans;
+        int n=graph.size();
+        
+        vector<int> visited( n,0);
+        vector<int> path;
+         
+        sourceToPath(graph, 0, n, visited,path, ans);
+        return ans;
+    }
+    
+    void sourceToPath(vector<vector<int>>& graph, int currNode, int n, vector<int> &visited, vector<int> currPath, vector<vector<int> > &ans   )
+    {
+        if( currNode== n-1){
             currPath.push_back(currNode);
             ans.push_back(currPath);
+            // backtracking the while returning
             currPath.pop_back();
-            return;
+            return ;
+            
             
         }
         if(visited[currNode]){
             return;
         }
-        
-        visited[currNode]=false;
+        visited[currNode]=1;
         currPath.push_back(currNode);
         
-        for( auto neighbour: graph[currNode]){
-            sourceToPath(graph,neighbour,visited,n,currPath,ans);
+        for ( auto neighour:graph[currNode]){
+            sourceToPath(graph,neighour,n, visited,currPath, ans);
         }
-        visited[currNode]=false;
+        visited[currNode]=0;
         currPath.pop_back();
-    }
-public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
-        
-        int n = graph.size();
-        
-        vector<bool> visited(n);
-        vector<int> currPath;
-        
-        sourceToPath( graph,0,visited,n,currPath,ans);
-        
-        return ans;
-        
+        return;
     }
 };
