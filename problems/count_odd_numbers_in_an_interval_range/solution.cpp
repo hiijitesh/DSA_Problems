6df379cca4fd92eae8d40b0d2d4248c9596cac1c
Jitesh Kumar <jitesbharti@gmail.com>
2022-03-03 03:00:46 +0000

    Sync LeetCode submission - Count Odd Numbers in an Interval Range (cpp)

---
+++ problems/count_odd_numbers_in_an_interval_range/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        
+        int cnt=0;
+         
+        for( int i=low; i<=high; i++){
+            if(i % 2 != 0)
+                cnt++;
+            else
+                continue;
+        }
+        return cnt;
+        
+    }
+};
