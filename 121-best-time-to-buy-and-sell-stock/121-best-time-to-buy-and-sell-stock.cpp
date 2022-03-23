3d04f4118e7581593388d799fdc843f549cbceba
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-03-23 22:08:04 +0530

    Time: 1315 ms (5.09%), Space: 269.7 MB (5.75%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private: 
+    int totalProfit(vector<int> &prices, int currDay, bool canBuy, int TxnCount, vector<vector<vector<int>>> &dp){
+        
+        if(currDay >= prices.size()) return 0;
+        if(TxnCount == 0) return 0;
+        if(dp[currDay][canBuy][TxnCount] != -1) return dp[currDay][canBuy][TxnCount];
+        
+        int idle = totalProfit(prices, currDay+1, canBuy, TxnCount, dp);
+        int buy = 0;
+        if(canBuy){
+             buy = -prices[currDay] + totalProfit(prices, currDay+1, false, TxnCount, dp);
+            return dp[currDay][canBuy][TxnCount] = max( idle, buy);
+        }
+        
+        else{
+            int sell = prices[currDay] + totalProfit(prices, currDay+1, true, TxnCount-1, dp);
+        
+            return dp[currDay][canBuy][TxnCount] = max( idle, sell);
+        }
+            
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(2,vector<int>(2,-1)));
+        return totalProfit(prices, 0, true, 1, dp);
+        
+    }
+};
