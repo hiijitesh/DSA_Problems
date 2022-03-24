72539db2a167741266e3309f70673ee21f613aeb
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-03-24 21:57:19 +0530

    Time: 425 ms (28.30%), Space: 17.9 MB (9.27%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size()+1, -1);
+     return isPossible(nums, 0, dp);
+
+    }
+   private: 
+      int isPossible(vector<int> &nums, int currIndex,  vector<int> &dp){
+        if(currIndex >= nums.size()-1) return 0;
+        // if( nums[currIndex] <= 0) return 0;
+          
+        if(dp[currIndex] != -1) return dp[currIndex];
+        int ans = 10001;
+        
+        int currJump = nums[currIndex];
+        for( int i=1; i<= currJump; i++){
+            
+             int tempAns= 1 + isPossible(nums, currIndex + i, dp);
+            
+             ans = min(ans , tempAns );
+        }
+        return dp[currIndex] = ans;
+    }
+};
