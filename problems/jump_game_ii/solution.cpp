73c944c037c3cd63e488cbb94bd79387f2b4c6ad
Jitesh Kumar <jitesbharti@gmail.com>
2022-03-24 16:41:34 +0000

    Sync LeetCode submission - Jump Game II (cpp)

--- problems/jump_game_ii/solution.cpp
+++ problems/jump_game_ii/solution.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    
-      int jumpSolver( vector<int> & nums, int CI, vector<int> &dp){
-        
-        if ( CI >= nums.size()-1 ) return 0;
-          
-        int key = CI;
-          
-        if(dp[key] != -1) return dp[key];
-        
-        int currJump= nums[CI];
-        
-        int  ans= 10001;
-        
-        for( int i=1; i<=currJump; i++ ){
-            
-            int tempAns= 1 + jumpSolver( nums, CI+ i, dp);
-            
-            ans = min(ans , tempAns );
-        }
-        
-        return dp[key] =ans;
-    }
-    
     int jump(vector<int>& nums) {
-        
         vector<int> dp(nums.size()+1, -1);
+        return isPossible(nums, 0, dp);
+
+    }
+   private: 
+      int isPossible(vector<int> &nums, int currIndex,  vector<int> &dp){
         
-        return jumpSolver( nums,0,dp);
+          if(currIndex >= nums.size()-1) return 0;
+          if(nums[currIndex] == 0) return 10001;
+          
+          if(dp[currIndex] != -1) return dp[currIndex];
+          int ans = 10001;
         
+          int currJump = nums[currIndex];
+          for( int i=1; i<= currJump; i++){
+            
+               int tempAns= 1 + isPossible(nums, currIndex + i, dp);
+            
+               ans = min(ans , tempAns );
+          }
+          
+         return dp[currIndex] = ans;
     }
 };
