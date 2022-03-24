59a05454015f144e7335c7df61fc75e4f45e5adb
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-03-24 17:50:05 +0530

    Time: 959 ms (5.06%), Space: 269.7 MB (5.76%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,29 +1,31 @@
-class Solution {
+class Solution { 
+public: 
+    int maxProfit(vector<int>& prices) { 
+        
+        // here I have created 3d vector in normal way 
+        vector<vector<vector<int>>> dp(prices.size()+1, vector<vector<int>>(2,vector<int>(2,-1)));
+        return maxProfitFromStocks(prices, 0, 1, 1, dp); 
+    } 
+     
 private: 
-    int totalProfit(vector<int> &prices, int currDay, bool canBuy, int TxnCount, vector<vector<vector<int>>> &dp){
-        
-        if(currDay >= prices.size()) return 0;
-        if(TxnCount == 0) return 0;
-        if(dp[currDay][canBuy][TxnCount] != -1) return dp[currDay][canBuy][TxnCount];
-        
-        int idle = totalProfit(prices, currDay+1, canBuy, TxnCount, dp);
-        int buy = 0;
-        if(canBuy){
-             buy = -prices[currDay] + totalProfit(prices, currDay+1, false, TxnCount, dp);
-            return dp[currDay][canBuy][TxnCount] = max( idle, buy);
-        }
-        
-        else{
-            int sell = prices[currDay] + totalProfit(prices, currDay+1, true, TxnCount-1, dp);
-        
-            return dp[currDay][canBuy][TxnCount] = max( idle, sell);
-        }
-            
-    }
-public:
-    int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(2,vector<int>(2,-1)));
-        return totalProfit(prices, 0, true, 1, dp);
-        
-    }
+    int maxProfitFromStocks(vector<int>& prices, int currentDay, int canBuy, int count,  vector<vector<vector<int>>> &dp){ 
+        if(currentDay >= prices.size() || count == 0) 
+            return 0; 
+         
+        int ans = 0; 
+        if(dp[currentDay][canBuy][count] != -1) 
+            return dp[currentDay][canBuy][count]; 
+        if(canBuy == 1){ 
+            int idle = maxProfitFromStocks(prices, currentDay + 1, canBuy, count, dp); 
+            int buy = - prices[currentDay] + maxProfitFromStocks(prices, currentDay+1, 0, count, dp); 
+            ans = max(idle, buy); 
+        } 
+        else{ 
+             int idle = maxProfitFromStocks(prices, currentDay + 1, canBuy, count, dp); 
+             int sell = prices[currentDay] + maxProfitFromStocks(prices, currentDay + 1, 1, count-1, dp); 
+             ans =  max(idle, sell); 
+        } 
+        dp[currentDay][canBuy][count] = ans; 
+        return dp[currentDay][canBuy][count]; 
+        } 
 };
