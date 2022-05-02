8f49103696253002142c56f03b800875b9bf512a
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-05-02 17:35:11 +0530

    Time: 203 ms (56.91%), Space: 59.4 MB (34.23%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int nearestValidPoint(int x, int y, vector<vector<int>>& points)
-    {
-        
-        int index = -1;
-        
-        int ans = INT_MAX;
-        
-        for(int i = 0; i< points.size(); i++)
-        {
-            
-            if(points[i][0] == x || points[i][1] == y)
-            {
-                int manhattan_dist = abs( x- points[i][0]) + abs(y- points[i][1]);
-                
-                if(manhattan_dist < ans)
-                {
-                    ans = manhattan_dist;
-                    index = i;
+    int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+        // support variables
+        int res = -1, bestDist = INT_MAX;
+        // parsing points
+        for (int i = 0, cx, cy, currDist, lmt = points.size(); i < lmt; i ++) {
+            // assigning cx and cy
+            cx = points[i][0], cy = points[i][1];
+            // looking for a match
+            if (x == cx || y == cy) {
+                // computing currDist and in case updating bestDist and res
+                currDist = abs(y - cy) + abs(x - cx);
+                if (currDist < bestDist) {
+                    bestDist = currDist, res = i;
                 }
             }
         }
-        return index;
-        
+        return res;
     }
 };
