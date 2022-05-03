dfbf7c50570994136ef35a8f98eeca15bfe595bb
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-05-03 12:37:52 +0530

    Time: 9 ms (5.79%), Space: 5.9 MB (48.80%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,9 +4,9 @@ public:
         
         int cnt = 0;
         while(n){
-            if((n&1) > 0)
+            if((n&1) > 0)// right shift each bit and count them
                 cnt++;
-            n = n>>1;
+            n = n>>1; // doing left shit to count binary 1
         }
         return cnt;
     }
