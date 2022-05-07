83869e61b326a46423798e2b4acf5a9b17d5fc9a
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-05-07 10:57:40 +0530

    Time: 7 ms (9.52%), Space: 7 MB (72.61%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -15,7 +15,7 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
         
-        while(fast != NULL && fast->next != NULL){
+        while(fast != nullptr && fast->next != nullptr){
             
             slow = slow->next;
             fast = fast->next->next;
