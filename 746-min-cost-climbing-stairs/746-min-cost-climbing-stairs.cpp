1fbe113c2248955405469c470cea3f721052c714
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-07-11 00:40:29 +0530

    Time: 8 ms (68.42%), Space: 13.9 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -2,22 +2,22 @@ class Solution {
     
 private:
     
-    int minCost( vector<int> &cost, int currStep, vector<int> &dp){
+    int minCost(vector<int> &cost, int currStep, vector<int> &dp){
         
-        if( currStep == cost.size()) return 0;
-        if( currStep > cost.size()) return 1002;
+        if(currStep == cost.size()) return 0;
+        if(currStep > cost.size()) return 1002;
         
-        if( dp[currStep] != -1) return dp[currStep];
+        if(dp[currStep] != -1) return dp[currStep];
         
-        int oneStepClimb = cost[currStep] + minCost( cost, currStep+1, dp);
-        int twoStepClimb = cost[currStep] + minCost( cost, currStep+2, dp);
-        
-        return dp[currStep] = min( oneStepClimb, twoStepClimb);
+        int oneStepClimb = cost[currStep] + minCost(cost, currStep+1, dp);
+        int twoStepClimb = cost[currStep] + minCost(cost, currStep+2, dp);
+
+        return dp[currStep] = min(oneStepClimb, twoStepClimb);
     }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         
         vector<int> dp(cost.size()+1, -1);
-        return min(minCost( cost, 0, dp), minCost(cost, 1, dp));
+        return min(minCost(cost, 0, dp), minCost(cost, 1, dp));
     }
 };
