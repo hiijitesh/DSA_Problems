8ad13578a3ed8773e221c29c0f331b264fe5e5cf
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-08-23 21:31:40 +0530

    Time: 266 ms (86.81%), Space: 114 MB (91.66%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -12,20 +12,64 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         
-        vector<int> ans;
+        if(head == NULL)
+            return false;
+        if(head->next == NULL)
+            return true;
         
-        ListNode* currNode = head;
-        while(currNode != NULL){
-
-            ans.push_back(currNode->val);
-            currNode = currNode->next;
+        ListNode* midNode = findMid(head);
+        ListNode* secondLL = midNode->next;
+        midNode->next = NULL;
+        
+        ListNode* l2 = reverse(secondLL);
+        return compare(head, l2);   
+        
+    }
+     //mid node
+    ListNode* findMid(ListNode* head){
+        
+        ListNode* fast = head;
+        ListNode *slow = head;
+        
+        while(fast->next != NULL and fast->next->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+     // reverse
+    ListNode* reverse(ListNode* head){
+        
+        ListNode* curr = head;
+        ListNode* prev = NULL, *nextNode = NULL;
+        
+        while(curr != NULL)
+        {
+            nextNode = curr->next;
+            curr->next =prev;
+            prev = curr;
+            curr = nextNode;
         }
         
-        for(int i = 0; i < ans.size()/2; i++)
-        {
-            if(ans[i] != ans[ans.size()-1-i])
+        return prev;
+    }
+    
+    
+    //compare
+    bool compare(ListNode* l1, ListNode* l2){
+        
+        while(l1 != NULL and l2 != NULL){
+            
+            if(l1->val != l2->val)
                 return false;
+            
+            l1 = l1->next;
+            l2 = l2->next;
         }
         return true;
     }
+    
+   
+   
 };
