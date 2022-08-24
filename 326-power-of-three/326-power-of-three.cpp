b1fe906c892e678cba0c90d1bff284f861018437
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-08-24 10:10:19 +0530

    Time: 23 ms (84.22%), Space: 5.9 MB (72.69%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         
-        //Base case
+        if(n <= 0) return false;
+        
         if(n == 1) return true;
         
-        if(n<=0 or n % 3 != 0) return false;
+        if(n %3 == 0) return isPowerOfThree(n/3);
         
-        return isPowerOfThree(n / 3);
+        else
+            return false;
     }
 };
