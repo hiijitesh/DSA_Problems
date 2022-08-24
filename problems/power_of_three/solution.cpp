bbb0c1ea71e2c5d47ef8ab55596a5d6e53767781
Jitesh Kumar <jitesbharti@gmail.com>
2022-08-24 04:42:48 +0000

    Sync LeetCode submission - Power of Three (cpp)

--- problems/power_of_three/solution.cpp
+++ problems/power_of_three/solution.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         
-        //Base case
         if(n == 1) return true;
+        if(n <= 0 or n%3 != 0 ) return false;
         
-        if(n<=0 or n % 3 != 0) return false;
         
-        return isPowerOfThree(n / 3);
+        return isPowerOfThree(n/3);
+        
     }
 };
