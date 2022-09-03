e11c9d48a89e333564f20b1eb21363ebc6897367
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-09-03 16:26:34 +0530

    Time: 4 ms (58.93%), Space: 8.6 MB (49.91%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,37 +1,30 @@
-class Solution
-{
-    public:
-
-        void dfs(int num, int n, int k, vector<int> &ans)
-        {
-            if (n == 1)
-            {
-                ans.push_back(num);
-                return;
-            }
-
-            int num1 = num % 10;
-            if (num1 - k >= 0)
-            {
-                dfs(num *10 + (num1 - k), n - 1, k, ans);
-            }
-
-            if (k)
-            {
-                if (num1 + k < 10)
-                {
-                    dfs(num *10 + (num1 + k), n - 1, k, ans);
-                }
-            }
-        }
-    vector<int> numsSameConsecDiff(int n, int k)
+class Solution {
+public:
+    
+    void dfs(int num, int n, int k, vector<int> &ans)
     {
-
-        vector<int> ans;
-
-        for (int i = 1; i < 10; i++)
+        if(n == 1)
         {
-            dfs(i, n, k, ans);
+            ans.push_back(num);
+            return;
+        }
+        
+        int num1 = num%10;
+        if(num1 - k >= 0){
+            dfs(num*10+ (num1 - k), n-1, k, ans);
+        }
+    
+        if( k && num1 + k < 10){
+            dfs(num*10 +(num1 + k), n-1, k, ans);
+        }
+    }
+    vector<int> numsSameConsecDiff(int n, int k) {
+        
+        vector<int> ans;
+        
+        for(int i = 1; i <10; i++)
+        {
+            dfs(i, n, k , ans);
         }
         return ans;
     }
