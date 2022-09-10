f564aeea863e2eadd7376c36a8edff7eac848fa6
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-09-10 08:33:06 +0530

    Time: 21 ms (48.09%), Space: 13.3 MB (13.90%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,23 +1,27 @@
 class Solution {
 private: 
-    int totalProfit(vector<int> &prices, int currDay, bool canBuy, int TxnCount, vector<vector<vector<int>>> &dp){
+    int totalProfit(vector<int> &prices, int currDay, bool canBuy, int TxnCount, vector<vector<vector<int>>> &dp)
+    {
+        if(currDay >= prices.size()) 
+            return 0;
+        if(TxnCount <= 0) 
+            return 0;
         
-        if(currDay >= prices.size()) return 0;
-        if(TxnCount <= 0) return 0;
         if(dp[currDay][canBuy][TxnCount] != -1) return dp[currDay][canBuy][TxnCount];
         
         int idle = totalProfit(prices, currDay+1, canBuy, TxnCount, dp);
-        int buy = 0;
+        
         if(canBuy){
-             buy = -prices[currDay] + totalProfit(prices, currDay+1, false, TxnCount, dp);
-             return dp[currDay][canBuy][TxnCount] = max( idle, buy);
+            int buy = -prices[currDay] + totalProfit(prices, currDay+1, false, TxnCount, dp);
+                 return dp[currDay][canBuy][TxnCount] = max(idle, buy);
+        }
+        else
+        {
+            int sell = prices[currDay] + totalProfit(prices, currDay+1, true, TxnCount-1, dp);
+                return dp[currDay][canBuy][TxnCount] = max(idle, sell);
+        
         }
         
-        else{
-            int sell = prices[currDay] + totalProfit(prices, currDay+1, true, TxnCount-1, dp);
-            return dp[currDay][canBuy][TxnCount] = max( idle, sell);
-        }
-            
     }
 public:
     int maxProfit(int k, vector<int>& prices) {
