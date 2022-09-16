281f71b86b400fd357f573b8075b44b0dd8f43fb
Jitesh Kumar <jitesbharti@gmail.com>
2022-09-16 16:31:25 +0000

    Sync LeetCode submission - Subarray Sum Equals K (cpp)

---
+++ problems/subarray_sum_equals_k/solution.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    public:
+        int subarraySum(vector<int> &nums, int k)
+        {
+            unordered_map<int, int> mp;
+            int sum = 0, ans = 0;
+            mp[sum] = 1;
+            for (auto it: nums)
+            {
+                sum += it;
+                int x = sum - k;
+                if (mp.find(x) != mp.end())
+                {
+                    ans += mp[x];
+                }
+                mp[sum]++;
+            }
+            return ans;
+        }
+};
