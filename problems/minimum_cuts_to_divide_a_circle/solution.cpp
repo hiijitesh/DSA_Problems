c1705b3d0d66442cdd0c53562343fd9c560cce15
Jitesh Kumar <jitesbharti@gmail.com>
2022-11-26 14:40:17 +0000

    Sync LeetCode submission - Minimum Cuts to Divide a Circle (cpp)

---
+++ problems/minimum_cuts_to_divide_a_circle/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        
+        
+        if (n == 1) return 0;
+        
+        if (n % 2 == 0) return n/2;
+        
+        else
+            return n;
+        
+//         if (n == 2 ) return 1;
+        
+//         if (n == 3) return 3;
+        
+//         if (n == 4) return 2;
+        
+        
+        
+        
+        
+        
+    }
+};
