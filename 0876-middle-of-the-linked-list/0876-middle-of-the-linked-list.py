efa8041b3681c9f0becd9cc330b3b3e6a0e244de
Jitesh Kumar <23386075+hiijitesh@users.noreply.github.com>
2022-12-05 11:30:44 +0530

    Time: 26 ms (98.56%), Space: 13.8 MB (55.96%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -1,27 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        ListNode* fast  = head, *slow = head;
+        fast = head
+        slow = head
         
-        while(fast!= NULL && fast->next!= NULL)
-        {
-            fast = fast->next->next;
-            slow = slow ->next;
-        }
+        while fast and fast.next:
+            
+            fast = fast.next.next
+            slow = slow.next
+            
         
+        return slow
+    
         
-        return slow;
-        
-    }
-};
