baead165554dc536cf84d1ed4d1578fce3aae621
Jitesh Kumar <jitesbharti@gmail.com>
2023-01-30 08:48:47 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

---
+++ problems/longest_consecutive_sequence/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        int ans = 0;
+        unordered_set<int> s;
+        
+        for(auto num:nums)
+            s.insert(num);
+        
+        for(auto num : nums)
+        {
+            if(!s.count(num-1))
+            {
+                int currNum = num;
+                int currLen = 1; // there will be atleast size of one sequence
+                
+                while(s.count(currNum+1))
+                {
+                    currNum+=1;
+                    currLen+=1;
+                }
+                
+                ans = max(ans, currLen);
+            }
+        }
+        
+        return ans;
+    }
+};
