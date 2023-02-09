3a126421c89e6748ad9015fa22221a792dded012
Jitesh Kumar <jitesbharti@gmail.com>
2023-02-10 00:18:36 +0530

    Time: 140 ms (61.42%), Space: 57.6 MB (43.94%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
-    bool isPalin(string &s, int low, int high)
+    vector<vector<string>> partition(string s) {
+        
+        vector<vector<string>> ans;
+        vector<string>curr;
+        solve(s, 0, s.size()-1, curr, ans);
+        return ans;
+        
+    }
+    bool isPalindrom(string s, int start, int end)
     {
-        while(low<=high)
+        while(start <= end)
         {
-            if(s[low]!=s[high])
+            if(s[start] != s[end])
                 return false;
-            
-            low++;
-            high--;
+            start++;
+            end--;
         }
         
         return true;
     }
-    void solve(int start, int end, string &s, vector<string>&temp, vector<vector<string>>&ans)
+    
+    void solve(string s, int start, int end, vector<string>&curr, vector<vector<string>> &ans )
     {
-        if(start>end)
+        if(start > end)
         {
-            ans.push_back(temp);
+            ans.push_back(curr);
             return;
-        }      
-                
-        for(int i = start;i<=end;i++)
+        }
+            
+        for(int i = start; i <= end; i++)
         {
-            if(isPalin(s,start,i))
+            if(isPalindrom(s, start, i))
             {
-                temp.push_back(s.substr(start,i-start+1));
-                solve(i+1,end,s,temp,ans);
-                temp.pop_back();
-            }            
+                curr.push_back(s.substr(start, i - start + 1));
+                solve(s, i+1, end, curr, ans);
+                curr.pop_back();
+            }
         }
         
         return;
     }
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
-        vector<string>temp;
-        solve(0,s.size()-1,s,temp,ans);
-        return ans;
-        
-    }
+    
+    
 };
