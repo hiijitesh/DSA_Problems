274b38895e19c418f99e6314becce05fde864ab5
Jitesh Kumar <jitesbharti@gmail.com>
2023-04-17 18:05:08 +0530

    Time: 19 ms (51.61%), Space: 13.7 MB (50.00%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,27 +1,28 @@
+
+    bool cmp(const pair<int, int> &a, const pair<int, int> &b){
+        
+    
+        return a.second > b.second;
+    }
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
-        map<int, int> mp;
         vector<int> ans;
+        unordered_map<int, int> mp;
         
         for(auto &x:nums)
             mp[x]++;
         
-        // define custom comparator function to sort by value in decreasing order
-        auto cmp = [](const pair<int, int>& a, const pair<int, int>& b) {
-            return a.second > b.second;
-        };
+        vector<pair<int, int>> smap(mp.begin(), mp.end());
+        sort(smap.begin(), smap.end(), cmp);
         
-        // create a vector of pairs from the map and sort it using the custom comparator
-        vector<pair<int, int>> pairs(mp.begin(), mp.end());
-        sort(pairs.begin(), pairs.end(), cmp);
         
-        // add the first k keys (which have the highest values) to the answer vector
-        for(int i = 0; i < k; i++) {
-            ans.push_back(pairs[i].first);
+        for(int i = 0; i < k; i++)
+        {
+            ans.push_back(smap[i].first);
         }
-        
         return ans;
+        
     }
 };
