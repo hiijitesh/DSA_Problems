c68483f39e514e81294ad6212b10e051a4ffb75b
Jitesh Kumar <jitesbharti@gmail.com>
2023-04-17 04:08:29 +0000

    Sync LeetCode submission - Contains Duplicate (cpp)

--- problems/contains_duplicate/solution.cpp
+++ problems/contains_duplicate/solution.cpp
@@ -1,37 +1,13 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) 
-    {
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
         
-        unordered_map<int, int> m;
+        for(auto &x:nums) mp[x]++;
         
-        for(int i=0; i<nums.size(); i++)  m[nums[i]]++;
-           
+        for(auto &e:mp) if (e.second >1) return true;
         
-        for(auto &i:m) if(i.second>=2) return true;                        
-               
-        return false; 
-            
-            
-        
-        
-       
-        
-        
-        
-//         int n = nums.size();
-        
-//         sort(nums.begin(), nums.end());
-        
-//        for ( int i=0; i<n-1; i++ ) 
-//        {
-           
-//            if(nums[i]==nums[i+1]) return true;
-//        }
-           
-       
-//                 return false;  
-                                               
+        return false;
         
     }
 };
