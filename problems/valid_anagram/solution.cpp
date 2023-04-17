2654c4a53c94a3cf79b791917087cfae4bd236d7
Jitesh Kumar <jitesbharti@gmail.com>
2023-04-17 09:10:04 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- problems/valid_anagram/solution.cpp
+++ problems/valid_anagram/solution.cpp
@@ -2,39 +2,23 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        unordered_map<char, int> mp;
-        
-       if(s.size() != t.size())
+        if(s.size() != t.size()) 
             return false;
         
-        for(int i = 0; i < s.size(); i++)
-        {
-            if(mp.find(s[i]) != mp.end())
-            {
-                mp[s[i]] += 1;
-            }
-            else
-            {
-                mp[s[i]] = 1;
-            }
-        }
+        unordered_map<int, int> mp;
         
-        
+        for(auto &e :s)
+            mp[e]++;
         
         for(int i = 0; i < t.size(); i++)
         {
             char currChar = t[i];
             
             if(mp.find(currChar) != mp.end())
-            {
-                mp[currChar] -= 1;
-                
-            }
+                mp[currChar]-=1;
             
             if(mp[currChar] == 0)
-                
                 mp.erase(currChar);
-            
         }
         
         return mp.size() == 0;
