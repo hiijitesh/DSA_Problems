c3014ce29019ec75c90a53ce5bc9f351459244dd
Jitesh Kumar <jitesbharti@gmail.com>
2023-04-18 14:06:44 +0530

    Time: 621 ms (26.37%), Space: 49.8 MB (59.22%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,29 +4,29 @@ public:
         
         unordered_set<int> s;
         
-        for(auto x : nums)
-            s.insert(x);
+        for(auto &x: nums) s.insert(x); //store the element in set so that we can check on later that wether particular element does exist in set or not
         
-        int ans = 0;
-        
+        int longestCount = 0;
         
         for(int i = 0; i < nums.size(); i++)
         {
-            int len = 0;
-            int currNum = nums[i] -1;
+            int  currCount = 0;
+            int currNum = nums[i]-1; // suppose curr num = 3, so will check 4,5,6,7 exist or not
             
             if(s.find(currNum) == s.end())
             {
-                while(s.find(currNum+1) != s.end())
-                {
-                    len++;
-                    currNum+=1;
-                }
-                
-                ans = max(ans, len);
+                //this means it will not check for the 5, 6, 7, it will direct start from the 9 or 10 
+            
+            while(s.find(currNum+1) != s.end())
+            {
+                currCount +=1;
+                currNum +=1;
             }
+            
+            longestCount = max(longestCount, currCount);
+            }
+            
         }
-        
-        return ans;
+        return longestCount;
     }
 };
