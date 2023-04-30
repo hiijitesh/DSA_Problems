527c864cc3708451e3c614f3a62bacb119bf4b2b
Jitesh Kumar <jitesbharti@gmail.com>
2023-04-30 13:31:46 +0530

    Time: 3 ms (79.99%), Space: 11.1 MB (42.21%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        
+        int start = 0, end = nums.size() - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            
+            
+            if(nums[mid] == target)
+                return mid;
+            
+            
+            //check if right side is stricly increasing
+            
+            if(nums[mid] >= nums[start])
+            {
+                //check if target does exists in left side strickly increasing side
+                
+                if(target <= nums[mid] && target >= nums[start])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            
+            //right side is stricly increasing
+            else
+            {
+                if(target >= nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+                
+            }
+        }
+        return -1;
+    }
+};
