9f3e164d92e1bd144866d9c4d41f9433a5b0d633
Jitesh Kumar <jitesbharti@gmail.com>
2023-05-06 18:37:21 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- problems/combination_sum/solution.cpp
+++ problems/combination_sum/solution.cpp
@@ -1,42 +1,36 @@
 class Solution {
-    
-private:
-    
-    void totalUniqueCombinations(vector<int>& nums, int currentIndex, int target, vector<int> &currentSet, vector<vector<int>> &result){
-        
-        //base case
-        // we we reached at last index or our target became zero
-        if(target == 0){
-            result.push_back(currentSet);
-            return;
-        }
-        if(currentIndex >= nums.size()) {
-            return;
-        }
-        
-        
-        
-        int currentValue = nums[currentIndex];
-        
-        //consider branch
-        if(currentValue <= target){
-            currentSet.push_back(currentValue);
-            totalUniqueCombinations(nums, currentIndex, target-currentValue, currentSet, result);
-            currentSet.pop_back();
-        }
-        
-        
-        //do not consider 
-        totalUniqueCombinations(nums, currentIndex+1, target, currentSet, result);
-        
-        
-    }
 public:
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        vector<vector<int>> result;
-        vector<int> currentSet;
+    void solve(vector<int>& nums, int currIdx,int target, vector<int>&validSet,  vector<vector<int>> &ans){
         
-        totalUniqueCombinations(nums, 0, target, currentSet, result);
-        return result;
+        if(target == 0)
+        {
+            ans.push_back(validSet);
+            return;
+        }
+        
+        if(currIdx >= nums.size())
+        {
+             return;
+        }
+        
+        //take current element 
+        int currNum = nums[currIdx];
+        
+        if(currNum <= target)
+        {
+            //take currNum into validset
+            validSet.push_back(currNum);
+            solve(nums, currIdx, target-currNum, validSet, ans);
+            validSet.pop_back();
+        }
+         solve(nums, currIdx+1, target, validSet, ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        
+        vector<vector<int>> ans;
+        vector<int>validSet;
+        
+        solve(nums, 0, target, validSet, ans);
+        return ans;
     }
 };
