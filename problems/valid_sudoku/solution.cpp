23dc8f8f08289580049b2986be35d6e50543fb06
Jitesh Kumar <jitesbharti@gmail.com>
2023-05-11 16:29:50 +0000

    Sync LeetCode submission - Valid Sudoku (cpp)

--- problems/valid_sudoku/solution.cpp
+++ problems/valid_sudoku/solution.cpp
@@ -13,6 +13,7 @@ public:
                     continue;
                 
                 int num = board[i][j] - '0';
+                cout<<num<<" "<<endl;
                 int box_c = ((i/3)*3) + (j/3);
                 
                 if(row[i][num] or col[j][num] or box[box_c][num])
