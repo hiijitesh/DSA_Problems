2f14bacc562b4909a8444220214006015c04ad15
Jitesh Kumar <jitesbharti@gmail.com>
2023-05-19 10:59:17 +0000

    Sync LeetCode submission - Subtree of Another Tree (cpp)

---
+++ problems/subtree_of_another_tree/solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* s, TreeNode* t)
+    {
+        if(!s || !t)
+           return s==NULL && t==NULL;
+         else if(s->val == t->val) 
+           return isSameTree(s->left,t->left) && isSameTree(s->right,t->right);
+       else
+           return false;
+    }
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        if(s == NULL)
+            return false;
+
+        else if(isSameTree(s,t))
+            return true;
+        else
+            return isSubtree(s->left, t) || isSubtree(s->right, t);
+    }
+};
