90f8ec3c284d033daadc6972f2b15c00c5dd16c1
Jitesh Kumar <jitesbharti@gmail.com>
2023-06-10 15:34:33 +0000

    Sync LeetCode submission - Sliding Window Maximum (cpp)

---
+++ problems/sliding_window_maximum/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        if(k>nums.size())
+        {
+            ans.push_back(*max_element(nums.begin(),nums.end()));
+            return ans;
+        }
+
+        deque<int>q;
+        int i=0,j=0;
+        while(j<nums.size())
+        {
+            while(!q.empty() && q.back()<nums[j]) 
+                q.pop_back();
+
+            q.push_back(nums[j]);
+            //move the j pointer
+            if(j-i+1<k) j++;
+
+            //windows size === k
+            else if(j-i+1==k){
+                ans.push_back(q.front());
+
+                if(q.front()==nums[i]) 
+                    q.pop_front();
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
