f94e194fb6d1d81b064d37740bd9de4098fe711d
Jitesh Kumar <jitesbharti@gmail.com>
2023-06-14 19:24:36 +0000

    Sync LeetCode submission - Top K Frequent Elements (cpp)

--- problems/top_k_frequent_elements/solution.cpp
+++ problems/top_k_frequent_elements/solution.cpp
@@ -1,28 +1,25 @@
-bool cmp(const pair<int, int> &a, const pair<int, int> &b){
-        
-    
-        return a.second > b.second;
-}
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
         unordered_map<int, int> mp;
         vector<int> ans;
+        priority_queue<pair<int, int>> maxheap;
 
-        for(auto &num:nums) 
+        for(auto &num:nums)
             mp[num]++;
 
-
-        vector<pair<int, int>> smap(mp.begin(), mp.end());
-        sort(smap.begin(), smap.end(), cmp);
-
-
-        for(int i = 0; i < k; i++)
-            ans.push_back(smap[i].first);
-
+        //store the pair of {freq, ele} in heap until heap size < map.size - k
+        //why map.size -k??? coz if we need k = 2 then we need to  populate the k frequent element 
+        for(auto &m:mp)
+        {
+            maxheap.push({m.second, m.first});
+            while(maxheap.size() > (mp.size() - k))
+            {
+                ans.push_back(maxheap.top().second);
+                maxheap.pop();
+            }
+        }
         return ans;
-
-
-
     }
 };
