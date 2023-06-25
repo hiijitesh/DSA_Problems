36e80161991ff6442c1fc4d68b2dd5489e8ec350
Jitesh Kumar <jitesbharti@gmail.com>
2023-06-25 17:33:20 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (cpp)

--- problems/best_time_to_buy_and_sell_stock/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock/solution.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
+    int bestTime(vector<int>& prices, int currDay, bool canBuy, int txn,vector<vector<vector<int>>>& dp)
+    {
+        //if curr day is out of index or total transaction is zero then return 0;
+
+        if(currDay >= prices.size())
+            return 0;
+
+        if(txn <= 0)
+            return 0;
+
+        if(dp[currDay][canBuy][txn] != -1)
+            return dp[currDay][canBuy][txn];
+
+        //if canBy == true then make 2 call--> either do nothing or Buy
+        if(canBuy)
+        {
+            int idle = bestTime(prices, currDay+1, canBuy, txn, dp);
+            int buy = -prices[currDay] + bestTime(prices, currDay+1, false, txn, dp);
+            return dp[currDay][canBuy][txn] = max(idle, buy);
+        }
+        else
+        {
+            //now we can sell or be idle , dont sell untill u got best day to sell for max profit
+            int idle = bestTime(prices, currDay+1, canBuy,txn, dp);
+            int sell = prices[currDay] + bestTime(prices, currDay+1, true, txn-1, dp);
+            return dp[currDay][canBuy][txn] = max(idle, sell);
+        }
+    }
     int maxProfit(vector<int>& prices) {
         
-        int l = 0, r = 1, n = prices.size()-1;
-        int profit = 0;
-        
-        while( r <= n)
-        {
-            if(prices[l] < prices[r])
-            {
-                int currProfit = prices[r] - prices[l];
-                profit = max(profit, currProfit);
-            }
-            
-            else
-                l = r;
-            
-            r++;
-        }
-        return profit;
+        vector<vector<vector<int>>> dp(prices.size()+1, vector<vector<int>>(2, vector<int>(2, -1)));
+        return bestTime(prices, 0, true, 1, dp);
     }
 };
