a891a7eab32f968850b385e6af2b57f102c9fdee
Jitesh Kumar <jitesbharti@gmail.com>
2023-06-25 18:47:50 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (cpp)

--- problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     
-    int bestTime ( vector<int> & prices, int currDay, bool canBuy,int trans, vector<vector<vector<int>>> &dp){
+    int bestTime ( vector<int> & prices, int currDay, bool canBuy,int txns, vector<vector<vector<int>>> &dp){
         
         if (currDay >= prices.size() ) return 0; 
-        if ( trans <=0 ) return 0;
+        if ( txns <=0 ) return 0;
         
-        if(dp[currDay][canBuy][trans] != -1) return dp[currDay][canBuy][trans];
+        if(dp[currDay][canBuy][txns] != -1) return dp[currDay][canBuy][txns];
         
         if(canBuy) {
-              int  idle = bestTime(prices,currDay+1,canBuy,trans,dp);
-              int  buy = -prices[currDay] + bestTime(prices, currDay+1,false,trans,dp);
-              return dp[currDay][canBuy][trans] = max(idle,buy);          
+              int  idle = bestTime(prices,currDay+1,canBuy,txns,dp);
+              int  buy = -prices[currDay] + bestTime(prices, currDay+1,false,txns,dp);
+              return dp[currDay][canBuy][txns] = max(idle,buy);          
         }
         else{
-             int idle = bestTime(prices,currDay+1,canBuy,trans,dp);
-             int sell = prices[currDay] + bestTime( prices,currDay+1,true,trans-1,dp);
-             return dp[currDay][canBuy][trans] = max(idle,sell);
+             int idle = bestTime(prices,currDay+1,canBuy,txns,dp);
+             int sell = prices[currDay] + bestTime( prices,currDay+1,true,txns-1,dp);
+             return dp[currDay][canBuy][txns] = max(idle,sell);
         }    
     }
     
