8797baaee232b9cb2776f3eddaf6824ea7f11727
Jitesh Kumar <jitesbharti@gmail.com>
2023-07-23 17:48:22 +0000

    Sync LeetCode submission - Permutation in String (cpp)

--- problems/permutation_in_string/solution.cpp
+++ problems/permutation_in_string/solution.cpp
@@ -5,29 +5,31 @@ public:
         if(s2.size() < s1.size())
             return false;
 
-        vector<int> mp1(26, 0), mp2(26,0);
-        for(auto &ch:s1)
+        vector<int> mp1(26, 0), mp2(26, 0);
+        for(auto ch:s1)
             mp1[ch-'a']++;
 
         int aq = 0, rel = 0;
 
-        while(aq < s2.size())
-        {
+        while(aq < s2.size()){
+            
             char curr = s2[aq];
             mp2[curr-'a']++;
-            int winSize = aq-rel+1;
-            if(winSize == s1.size())
-            {
+
+            int winLen = aq-rel+1;
+            // at valid windows
+            if(winLen == s1.size()){
                 if(mp1 == mp2)
                     return true;
             }
-            
-            if(winSize < s1.size()) aq++;
-            else {
+            if(winLen < s1.size())
+                aq++;
+            else{
                 mp2[s2[rel]-'a']--;
                 rel++;
                 aq++;
             }
+            // aq++;
         }
         return false;
     }
