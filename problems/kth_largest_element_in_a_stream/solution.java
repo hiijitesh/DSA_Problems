a5d3b0f1b613d1da16058461d4c8d237b6245c30
Jitesh Kumar <jitesbharti@gmail.com>
2023-08-09 18:12:14 +0000

    Sync LeetCode submission - Kth Largest Element in a Stream (java)

---
+++ problems/kth_largest_element_in_a_stream/solution.java
@@ -0,0 +1,21 @@
+class KthLargest {
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+		for (int i: nums) {
+			minHeap.add(i);
+			if (minHeap.size() > k) {
+				minHeap.poll();
+			}
+		}
+  }
+    
+    public int add(int val) {
+        minHeap.add(val);
+		if (minHeap.size() > k) {
+			minHeap.poll();
+		}
+		return minHeap.peek();
+    }
+}
