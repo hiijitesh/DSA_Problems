21a9d641599548d80e0f912aebb1499593550e51
Jitesh Kumar <jitesbharti@gmail.com>
2023-08-10 18:05:59 +0000

    Sync LeetCode submission - Subsets II (cpp)

--- problems/subsets_ii/solution.cpp
+++ problems/subsets_ii/solution.cpp
@@ -1,48 +1,28 @@
 class Solution {
+public:
+    void generateSubsets(int idx, vector<int>&nums, vector<int>&currset, set<vector<int>>&result){
 
-private: 
-    void generateAllSubsets(vector<int> &nums, int currentIndex, vector<int>&currentSet, set<vector<int>> &result){
-        
-        // Base case // if we reached at last index then we can push back aur current set to result
-        if(currentIndex >= nums.size()){
-            result.insert(currentSet);
+        if(idx >= nums.size()){
+            result.insert(currset);
             return;
         }
-        
-        //we wiil take one element from nums array 
-        int currentValue = nums[currentIndex];
-        
-        //if we consider that currentValue element then we push back into current set and move to next index 
-        currentSet.push_back(currentValue);
-        
-        //call to consider branch
-        generateAllSubsets(nums, currentIndex+1, currentSet, result);
-        
-        // while returning we have to pop back those element that was added last time because we are printing the subset so its should be dlted after printing
-        currentSet.pop_back();
-        
-        // call to not consider branch
-        generateAllSubsets(nums, currentIndex+1, currentSet, result);
-        
+
+        int val = nums[idx];
+        currset.push_back(val);
+        generateSubsets(idx+1, nums, currset, result);
+        currset.pop_back();
+        generateSubsets(idx+1, nums, currset, result);
     }
-    
-public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
-        //here we took set of vectors so that we can avoid the duplicate subset ok
-        set<vector<int>> result;
-        
-        vector<int> currentSet;
-        
-        //we need to sort the nums array so that it would be easy to filter duplicate subsets
+        set<vector<int>>result;
+        vector<int> currset;
+
         sort(nums.begin(), nums.end());
-        
-        generateAllSubsets(nums, 0, currentSet, result);
-        
-        // but we have to return the ans in form of vector of vector so that we converted our set into vector 
+        generateSubsets(0, nums, currset, result);
+
+        //need ans in vector
         vector<vector<int>> ans(result.begin(), result.end());
         return ans;
     }
-    
-    
 };
