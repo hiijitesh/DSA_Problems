d6d738d8c50964260f40d61edeb51210be354a3f
Jitesh Kumar <jitesbharti@gmail.com>
2023-08-26 12:11:53 +0000

    Sync LeetCode submission - House Robber (cpp)

--- problems/house_robber/solution.cpp
+++ problems/house_robber/solution.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        int prev = 0;
+        int curr = 0;
+        int next = 0;
         
-        int n= nums.size();
-        int dp[n+1];
-        
-        if( n <1 ) return 0;
-        if( n <2 ) return nums[0];
-        
-        dp[0] = nums[0];
-        dp[1] = max(dp[0], nums[1]);
-        
-        for( int i=2; i<n; i++){
-            //in first para what i did is i robbed that house so back of minus 2 house will also had robed, in second paara i assume that i have not robbed that house so just adjecnt back will get robbed
-            dp[i] = max( nums[i] + dp[i-2], dp[i-1]);
+        for (int i = 0; i < nums.size(); i++) {
+            next = max(prev + nums[i], curr);
+            prev = curr;
+            curr = next;
         }
-        return dp[n-1];
         
-        
+        return curr;
     }
 };
+
