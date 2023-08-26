95abaca24bcf7d2ab504ba37f4af0e0d887e0729
Jitesh Kumar <jitesbharti@gmail.com>
2023-08-26 12:10:21 +0000

    Sync LeetCode submission - House Robber II (cpp)

--- problems/house_robber_ii/solution.cpp
+++ problems/house_robber_ii/solution.cpp
@@ -1,33 +1,30 @@
-class Solution {
 
-    private:
-    
-    int maxMoney ( vector<int> &nums){
-         
-        int n= nums.size();
-        int dp[n+1];
-        
-        dp[0] = nums[0];
-        dp[1] = max(dp[0], nums[1]);
-        
-        for( int i=2; i<n; i++){
- 
-            dp[i] = max( nums[i] + dp[i-2], dp[i-1]);
-        }
-        return dp[n-1];
-    }
+class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=  nums.size();
-        if( n ==0 ) return 0;
-        if( n ==1 ) return nums[0];
-        if( n ==2 ) return max(nums[0], nums[1]);
+        int n = nums.size();
         
-        vector<int> firstCycle(nums.begin(), nums.end()-1);
-        vector<int> secondCycle(nums.begin()+1, nums.end());
+        if (n == 1) {
+            return nums[0];
+        }
         
-        return max(maxMoney(firstCycle), maxMoney(secondCycle));
+        int range1 = robber(nums, 0, n - 2);
+        int range2 = robber(nums, 1, n - 1);
         
-       
+        return max(range1, range2);
+    }
+private:
+    int robber(vector<int>& nums, int start, int end) {
+        int prev = 0;
+        int curr = 0;
+        int next = 0;
+        
+        for (int i = start; i <= end; i++) {
+            next = max(prev + nums[i], curr);
+            prev = curr;
+            curr = next;
+        }
+        
+        return curr;
     }
 };
