8a76549620a0240bd6abbe333ad52dc3d924c3b2
Jitesh Kumar <jitesbharti@gmail.com>
2023-10-11 14:54:44 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- problems/unique_paths/solution.cpp
+++ problems/unique_paths/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int path(int m, int n, int row , int col, vector<vector<int>> &dp){
-        
-        if ( row== m-1 && col == n-1) return 1;
-        
-        if ( row >= m || col >= n) return 0;
-        
-        if( dp[row][col] != -1 ) return dp[row][col];
-        
-        int down = path( m,n,row+1, col,dp);
-        
-        int right = path( m,n, row, col+1,dp);
-        
-        return dp[row][col] = down+right;
+    int totalPath(int m, int n, int row, int col, vector<vector<int>> &dp){
+        if(row == m-1 && col == n-1)
+            return 1;
+        if(row >= m || col >= n)
+            return 0;
+        if(dp[row][col] != -1)
+            return dp[row][col];
+
+        int downPathCount = totalPath(m, n, row+1, col, dp);
+        int rightPathCount = totalPath(m, n, row, col+1, dp);
+
+        return dp[row][col] = downPathCount + rightPathCount;
     }
     int uniquePaths(int m, int n) {
-        
-        vector<vector<int>> dp(101, vector<int> (101,-1));
-        
-        return path( m, n, 0, 0,dp);
-        
+        vector<vector<int>> dp(101, vector<int>(101, -1));
+        return totalPath(m, n, 0, 0, dp);
     }
 };
