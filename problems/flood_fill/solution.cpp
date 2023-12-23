f536e9dac0ba9da22f4bc34ac3324b7283d3b35a
Jitesh Kumar <jitesbharti@gmail.com>
2023-12-23 13:51:21 +0000

    Sync LeetCode submission - Flood Fill (cpp)

--- problems/flood_fill/solution.cpp
+++ problems/flood_fill/solution.cpp
@@ -1,27 +1,35 @@
 class Solution {
+
 private:
-    void dfs(vector<vector<int>>&image,int row, int col, int m,int n,int currColor, int newColor){
-        
-        if( col <0 || col >= n || row <0 || row>=m ||image[row][col] != currColor)
+    void dfs(vector<vector<int>>& image,int row, int col,  int currColor, int newColor){
+
+        int m = image.size(); // number of rows
+        int n = image[0].size(); // number of cols
+
+        if(row >= m || col >= n)
+            return;
+        if(row < 0 || col < 0)
+            return;
+        if(image[row][col] !=  currColor)
             return;
         
         image[row][col]=newColor;
         
-        dfs(image,row-1,col,m,n,currColor,newColor);
-        dfs(image,row,col+1,m,n,currColor,newColor);
-        dfs(image,row+1,col,m,n,currColor,newColor);
-        dfs(image,row,col-1,m,n,currColor,newColor);
+        dfs(image,row-1,col,currColor,newColor);
+        dfs(image,row,col+1,currColor,newColor);
+        dfs(image,row+1,col,currColor,newColor);
+        dfs(image,row,col-1,currColor,newColor);
         return;
+
 
     }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        
-        if(image[sr][sc]==newColor)
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,int color) {
+      int initialColor = image[sr][sc];
+      if(initialColor == color)
         return image;
-        
-        dfs(image,sr,sc,image.size(),image[0].size(),image[sr][sc],newColor);
-        return image;
-        
+
+      dfs(image, sr, sc, initialColor, color);
+      return image;
     }
 };
