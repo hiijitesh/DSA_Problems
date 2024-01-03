e27f63bd763ab7fbcff6a170afce2621ba61fb5f
Jitesh Kumar <jitesbharti@gmail.com>
2024-01-03 18:01:14 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int fib(int n) {
-        
-        int dp[n+1];
+    
+    int solve(int n, vector<int> &dp){
         
         if(n == 0) return 0;
-        if( n==1) return 1; 
+        if(n == 1) return 1;
         
-        dp[0]=0;
-        dp[1]=1;
+        if(dp[n] != -1) return dp[n];
+        if(dp[n-1] != -1) return dp[n-1];
         
-        for( int i= 2; i<=n; i++)
-        {
-            dp[i] = dp[i-2] + dp[i-1];
-        }
+        int fib1 = solve(n-1, dp);
+            
+        int fib2 = solve(n-2, dp);
         
-        return dp[n];
+        return dp[n] = fib1 + fib2;
+    }
+    int fib(int n) {
+        
+       vector<int> dp(n+1, -1);
+        
+        return solve(n, dp);
         
     }
 };
