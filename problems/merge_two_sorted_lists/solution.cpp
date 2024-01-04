43248bac6403eedd53b6be6eb3faa51a455ac90f
Jitesh Kumar <jitesbharti@gmail.com>
2024-01-04 16:47:42 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- problems/merge_two_sorted_lists/solution.cpp
+++ problems/merge_two_sorted_lists/solution.cpp
@@ -12,41 +12,19 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
-        ListNode* dummy = new ListNode(-100);
-        ListNode* head  = dummy;
+        if(!list1) return list2;
         
-        if(list1 == NULL) 
-            return list2;
+        if(!list2) return list1;
         
-        if(list2 == NULL)
+        if(list1->val <= list2->val){
+             list1->next = mergeTwoLists(list1->next, list2);
             return list1;
-        
-        while(list1 != NULL && list2 != NULL)
-        {
-            
-            if(list1 -> val < list2 -> val)
-            {
-                ListNode* tempNode = new ListNode(list1->val);
-                dummy->next = tempNode;
-                
-                list1 = list1->next;
-            }
-            else
-            {
-                ListNode* tempNode = new ListNode(list2->val);
-                dummy->next = tempNode;
-                list2 = list2->next;
-            }
-            dummy = dummy->next;
         }
         
-        //when list1 is empyt
+       else{
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
         
-        if(list2 != NULL)
-            dummy->next = list2;
-        if(list1 != NULL)
-            dummy->next = list1;
-        
-        return head->next;
     }
 };
