183fc5148899375506e150c232cbd720d2321530
Jitesh Kumar <jiteshece@gmail.com>
2024-12-26 19:53:43 +0530

    Time: 9 ms (96.38%) | Memory: 24.7 MB (75.88%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // first sort the current string, and put it as a key in map
-        // then insert og str as value
-        // eat ->sort-> aet(key). [eat] --->> val
+         vector<vector<string>> anagrams;
+         unordered_map<string, vector<string>>anagramCount;
 
-        //at last push all the value of map into res vector
+         for(int i = 0; i < strs.size(); i++){
 
-
-        vector<vector<string>> ans;
-        map<string, vector<string>> mp;
-
-        for(int i=0; i<strs.size(); i++){
             string currStr = strs[i];
-            //sort the strs[i] so it will act as unique key of map
-            sort(strs[i].begin(), strs[i].end());
-            mp[strs[i]].push_back(currStr);
-        }
+            sort(currStr.begin(), currStr.end());
 
-        for(auto ele:mp){
-            ans.push_back(ele.second);
-        }
-        return ans;
-        
+            anagramCount[currStr].push_back(strs[i]);
+            // if(anagramCount.find(currStr) != anagramCount.end()){
+            // }else{
+            //     anagramCount[currStr] = 
+            // }
+         }
+
+         for(auto& ele:anagramCount){
+            anagrams.push_back(ele.second);
+         }
+
+         return anagrams;
     }
 };
